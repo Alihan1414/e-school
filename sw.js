afc788d422615f04e039be1931dfa85d94bd0c52
Alihan Divanli <alihandivanli8@gmail.com>
2026-09-04 18:02:59 +0300

    fix(theme): guarantee 100% white crisp contrast and visibility in dark mode

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for E-School Daara (PWA & Offline-First Enterprise Caching)
-const CACHE_NAME = 'eschool-daara-v25';
+const CACHE_NAME = 'eschool-daara-v26';
 
 const ASSETS_TO_CACHE = [
   './',
